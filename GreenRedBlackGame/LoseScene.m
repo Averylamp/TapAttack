8e5c788f355c7f8a2ba6696995eba0a783602cd1
Avery Lamp <averylamp@gmail.com>
2015-01-02 01:41:13 -0500

    Animated Lose Screen

--- GreenRedBlackGame/LoseScene.m
+++ GreenRedBlackGame/LoseScene.m
@@ -37,8 +37,6 @@
     SKSpriteNode *backgroundButton = [[SKSpriteNode alloc]initWithImageNamed:@"loseScreenButtonBackground"];
     scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Condensed"];
     CGPoint position = CGPointMake(self.screenSize.width/2, self.screenSize.height- 280);
-    backgroundButton.name = @"score";
-    scoreLabel.name = @"score";
     backgroundButton.xScale = 2;
     backgroundButton.yScale = 1.8;
     backgroundButton.position = position;
@@ -46,13 +44,15 @@
     scoreLabel.fontSize = 80.0f;
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height- 310);
     scoreLabel.position = position;
+    backgroundButton.name = @"score";
+    scoreLabel.name = @"score";
+    self.scoreLabel=scoreLabel;
+    self.scoreBackground = backgroundButton;
     [self addChild:backgroundButton];
     [self  addChild:scoreLabel];
     
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height - 420);
     scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Condensed"];
-    backgroundButton.name = @"share";
-    scoreLabel.name = @"share";
     backgroundButton = [[SKSpriteNode alloc]initWithImageNamed:@"loseScreenButtonBackground"];
     backgroundButton.xScale = 2;
     backgroundButton.yScale = 1.8;
@@ -61,13 +61,15 @@
     scoreLabel.fontSize = 80.0f;
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height- 450);
     scoreLabel.position= position;
+    backgroundButton.name = @"share";
+    scoreLabel.name = @"share";
+    self.shareLabel = scoreLabel;
+    self.shareBackground = backgroundButton;
     [self addChild:backgroundButton];
     [self  addChild:scoreLabel];
     
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height - 560);
     scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Condensed"];
-    backgroundButton.name = @"home";
-    scoreLabel.name = @"home";
     backgroundButton = [[SKSpriteNode alloc]initWithImageNamed:@"loseScreenButtonBackground"];
     backgroundButton.xScale = 2;
     backgroundButton.yScale = 1.8;
@@ -76,13 +78,15 @@
     scoreLabel.fontSize = 80.0f;
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height- 590);
     scoreLabel.position= position;
+    backgroundButton.name = @"home";
+    scoreLabel.name = @"home";
+    self.homeLabel = scoreLabel;
+    self.homeBackground = backgroundButton;
     [self addChild:backgroundButton];
     [self  addChild:scoreLabel];
     
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height - 700);
     scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Condensed"];
-    backgroundButton.name = @"again";
-    scoreLabel.name = @"again";
     backgroundButton = [[SKSpriteNode alloc]initWithImageNamed:@"loseScreenButtonBackground"];
     backgroundButton.xScale = 2;
     backgroundButton.yScale = 1.8;
@@ -91,6 +95,10 @@
     scoreLabel.fontSize = 80.0f;
     position = CGPointMake(self.screenSize.width/2, self.screenSize.height- 730);
     scoreLabel.position= position;
+    backgroundButton.name = @"again";
+    scoreLabel.name = @"again";
+    self.againLabel = scoreLabel;
+    self.againBackground = backgroundButton;
     [self addChild:backgroundButton];
     [self  addChild:scoreLabel];
 }
@@ -99,18 +107,67 @@
     UITouch * touch = (UITouch *)[[touches allObjects]firstObject];
     CGPoint location = [touch locationInNode:self];
     SKNode *node = [self nodeAtPoint:location];
-    
+    SKAction *growb = [SKAction scaleXTo:2.4 y:2 duration:0.2];
+    SKAction *growl = [SKAction scaleTo:1.2 duration:0.2];
+    if([node.name isEqualToString:@"share"]){
+        [self.shareBackground runAction:growb];
+        [self.shareLabel runAction:growl];
+    }
+    if([node.name isEqualToString:@"home"]){
+        [self.homeBackground runAction:growb];
+        [self.homeLabel runAction:growl];
+    }
+    if([node.name isEqualToString:@"again"]){
+        [self.againBackground runAction:growb];
+        [self.againLabel runAction:growl];
+    }
     
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
+    UITouch * touch = (UITouch *)[[touches allObjects]firstObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    SKAction *shrinkb = [SKAction scaleXTo:2.0 y:1.8 duration:0.2];
+    SKAction *shrinkl = [SKAction scaleTo:1 duration:0.2];
+    if(![node.name isEqualToString:@"share"]){
+        [self.shareBackground runAction:shrinkb];
+        [self.shareLabel runAction:shrinkl];
+    }
+    if(![node.name isEqualToString:@"home"]){
+        [self.homeBackground runAction:shrinkb ];
+        [self.homeLabel runAction:shrinkl];
+    }
+    if(![node.name isEqualToString:@"again"]){
+        [self.againBackground runAction:shrinkb];
+        [self.againLabel runAction:shrinkl];
+    }
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
+    UITouch * touch = (UITouch *)[[touches allObjects]firstObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    SKAction *shrinkb = [SKAction scaleXTo:2.0 y:1.8 duration:0.2];
+    SKAction *shrinkl = [SKAction scaleTo:1 duration:0.2];
+    if([node.name isEqualToString:@"share"]){
+        
+    }else{
+        [self.shareBackground runAction:shrinkb];
+        [self.shareLabel runAction:shrinkl];
+    }
+    if([node.name isEqualToString:@"home"]){
+        [self.viewController menu:nil];
+    }else{
+
+    }
+    if([node.name isEqualToString:@"again"]){
+        [self.viewController play:nil];
+    }else{
+        
+    }
 }
 
 @end
