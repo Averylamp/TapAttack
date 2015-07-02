b6b2969c63ba5b8ed4da9207f27ebe4f515482df
Avery Lamp <averylamp@gmail.com>
2015-07-02 02:01:05 -0400

    Hopefully fixed preloading bug?

--- GreenRedBlackGame/MenuScene.m
+++ GreenRedBlackGame/MenuScene.m
@@ -265,7 +265,12 @@
     if([node.name isEqualToString:@"playButton"]){
         
         [self.playButton runAction: [SKAction scaleTo:2.5 * screenScale / 2 duration:0.1]];
-        [self.viewController play:nil];
+        int64_t delayInSeconds = 0.5;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self.viewController play:nil];
+        });
+        
         
     }else{
         [self.playButton runAction: [SKAction scaleTo:2.5 * screenScale / 2 duration:0.15]];
